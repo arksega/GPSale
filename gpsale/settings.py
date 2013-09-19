726a87167c54f78e3a2a56d380082eb1e177a6c5
Sergio Gutierrez <ark.sega@gmail.com>
2013-09-18 23:32:51 -0500

    Customising admin base view

--- gpsale/settings.py
+++ gpsale/settings.py
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'gpsale.urls'
 WSGI_APPLICATION = 'gpsale.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/sergio/gpsale/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
